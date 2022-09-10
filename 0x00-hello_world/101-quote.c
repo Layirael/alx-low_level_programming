6aaab9aae7e52200f931bc5cbaa535046366789f
Israel Olayiwola <oluwapelumimarisraeli@gmail.com>
2022-09-09 20:34:50 -0700

    size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,9 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
+* Description: using the main function 
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-	quote("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-	return (0);
-} 
