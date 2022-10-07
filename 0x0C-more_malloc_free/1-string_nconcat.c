d131e6c76bdb4cbef5387b8853fddae204cb23ce
Israel Olayiwola <oluwapelumimarisraeli@gmail.com>
2022-10-07 16:45:07 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (p == NULL)
 		return (0);
-	
+
 	for (i = 0; i < size1; i++)
 	{
 		p[i] = s1[i];
@@ -48,4 +48,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (p);
 }
-
